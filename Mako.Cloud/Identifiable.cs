8d2952f6082e1543c55f4fad3c4dc47dabef34b5
Matt Waldron <waldron.matthew@gmail.com>
2024-01-02 21:01:16 -0500

    adding cloud library

---
+++ Mako.Cloud/Identifiable.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Bson;
+
+namespace Mako.Cloud;
+
+public class Identifiable
+{
+    public ObjectId Id { get; set; }
+}
---
+++ Mako.Cloud/MongoDb.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Mako.Cloud;
+
+public class MongoDb<T> where T : Identifiable
+{
+    private IMongoCollection<T> _collection;
+    public MongoDb(string connectionString, string databaseName, string collectionName)
+    {
+        var client = new MongoClient(connectionString);
+        _collection = client.GetDatabase(databaseName).GetCollection<T>(collectionName);
+    }
+
+    public void Insert(T obj)
+    {
+        _collection.InsertOne(obj);
+    }
+
+    public IEnumerable<T> FindAll()
+    {
+        return _collection.AsQueryable<T>();
+    }
+
+    public void Delete(ObjectId id)
+    {
+        _collection.DeleteOne(a => a.Id == id);
+    }
+}
